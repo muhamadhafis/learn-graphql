0c3d2a4390193eb4e4a70b685320b30f3733eded
Muhamad Hafis <hafisgtngm199@gmail.com>
2025-08-17 12:16:05 +0700

    learn: single & multiple get data using param

--- _db.js
+++ _db.js
@@ -5,7 +5,7 @@ const books = [
     author: "Sam Raimi",
     publish_at: "2021-12-15",
     category: "Action",
-    price: 500
+    price: 500,
   },
   {
     id: 2,
@@ -13,23 +13,71 @@ const books = [
     author: "Sam Raimi",
     publish_at: "2021-12-15",
     category: "Action",
-    price: 500
-  }
-]
+    price: 500,
+  },
+];
 
 const members = [
   {
     id: 1,
     name: "John Doe",
-    email: "7VXbU@example.com",
-    verified: true
+    email: "john@example.com",
+    verified: true,
   },
   {
     id: 2,
     name: "Jane Doe",
-    email: "7VXbU@example.com",
-    verified: false
-  }
-]
+    email: "jane@example.com",
+    verified: false,
+  },
+];
 
-export { books, members }
+const products = [
+  {
+    id: 1,
+    name: "AC Dimmer 300W",
+    stock: 10,
+    location: "Warehouse",
+    sku: "1A1A1",
+    bundle: 100,
+    unit: "pcs",
+  },
+  {
+    id: 2,
+    name: "AC Dimmer 100W",
+    stock: 50,
+    location: "Warehouse",
+    sku: "1A1A2",
+    bundle: 100,
+    unit: "pcs",
+  },
+  {
+    id: 3,
+    name: "AC Dimmer 200W",
+    stock: 20,
+    location: "Warehouse",
+    sku: "1A1A3",
+    bundle: 100,
+    unit: "pcs",
+  },
+  {
+    id: 4,
+    name: "Thermal Memory Gray",
+    stock: 70,
+    location: "Store",
+    sku: "1A1",
+    bundle: 1,
+    unit: "pcs",
+  },
+  {
+    id: 5,
+    name: "Thermal Memory Black",
+    stock: 50,
+    location: "Store",
+    sku: "1A2",
+    bundle: 1,
+    unit: "pcs",
+  },
+];
+
+export { books, members, products };
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { books, members } from "./_db.js"
+import { books, members, products } from "./_db.js";
 
-const typeDefs =  `#graphql
+const typeDefs = `#graphql
   type Books{
     id: ID!
     title: String!
@@ -20,28 +20,43 @@ const typeDefs =  `#graphql
     verified: Boolean!
   }
 
+  type Products{
+    id: ID!
+    name: String!
+    stock: Int!
+    location: String
+    sku: String
+    bundle: Int!
+    unit: String!
+  }
+
   type Query {
     books: [Books!]!
+    book(id: ID!): Books!
     members: [Members!]!
-  }`
+    member(id: ID!): Members!
+    products: [Products!]!
+    product(id: ID, location: String): [Products!]!
+  }`;
 
 const resolvers = {
   Query: {
-    books: () => {
-      return books
-    },
-    members: () => {
-      return members 
-    }
-  }
-}
+    books: () => books,
+    book: (_, args) => books.find((book) => book.id == args.id),
+    members: () => members,
+    member: (_, args) => members.find((member) => member.id == args.id),
+    products: () => products,
+    product: (_, args) => products.find((p) => p.location == args.location),
+  },
+};
 
 const server = new ApolloServer({
-  typeDefs, resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-const {url} = await startStandaloneServer(server, {
-  listen: {port: 4000}
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-console.log(`🚀 Server ready at: ${url}`)
+console.log(`🚀 Server ready at: ${url}`);
